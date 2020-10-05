37b42b30ec0d7b65c9c1d2db8e049551690a6378
samsonemes <50766123+samsonemes@users.noreply.github.com>
2020-10-05 16:09:11 +0300

    Add files via upload

---
+++ precache-manifest.bb26d6ee6d0300c845cdb4801c774894.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b84d546adcc889d6af71863020ba57aa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c6421c4210c43894999e",
+    "url": "/static/css/main.0f4705b3.chunk.css"
+  },
+  {
+    "revision": "1f32c5e5cd717042473e",
+    "url": "/static/js/2.5a6882cb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5a6882cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c6421c4210c43894999e",
+    "url": "/static/js/main.f1604cb8.chunk.js"
+  },
+  {
+    "revision": "f16c43ef6a0656d529d1",
+    "url": "/static/js/runtime-main.12d63b17.js"
+  },
+  {
+    "revision": "b9c9fb9f41df038b4304ec8a927f4951",
+    "url": "/static/media/cd.b9c9fb9f.jpg"
+  },
+  {
+    "revision": "80665835291193dc307b733d5ecfccef",
+    "url": "/static/media/community.80665835.png"
+  },
+  {
+    "revision": "76c846db03dc71b163817d0dced56024",
+    "url": "/static/media/dancer.76c846db.png"
+  },
+  {
+    "revision": "4d1221c0a4c45bba6ed7cba9db0700a5",
+    "url": "/static/media/entertainment.4d1221c0.png"
+  },
+  {
+    "revision": "1edcb3d9a0955616ea23d689f0f33808",
+    "url": "/static/media/listen.1edcb3d9.webp"
+  },
+  {
+    "revision": "3f525af1f743d86e47729d99b92107fc",
+    "url": "/static/media/logo.3f525af1.svg"
+  },
+  {
+    "revision": "adcb2a98a52e781e24cfbfd29ca84d70",
+    "url": "/static/media/mziki.adcb2a98.png"
+  }
+]);
